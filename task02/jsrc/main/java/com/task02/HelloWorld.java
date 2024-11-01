1454a495ad581ec4aeb95cb009a4cb845ae62b5b
Harshil Kothari <harshil_kothari@epam.com>
2024-11-01 16:47:07 +0530

    2

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -1,9 +1,15 @@
 package com.task02;
 
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
 import com.syndicate.deployment.model.RetentionSetting;
+import com.syndicate.deployment.model.lambda.url.AuthType;
+import com.syndicate.deployment.model.lambda.url.InvokeMode;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,13 +21,35 @@ import java.util.Map;
 	aliasName = "learn",
 	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
-public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
+@LambdaUrlConfig(
+		authType = AuthType.NONE, // This means no authentication is required to access the Function URL
+		invokeMode = InvokeMode.BUFFERED // This can be set to BUFFERED or STREAMING based on your requirements
+)
+public class HelloWorld implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
 
-	public Map<String, Object> handleRequest(Object request, Context context) {
-		System.out.println("Hello from lambda");
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("statusCode", 200);
-		resultMap.put("message", "Hello from Lambda");
-		return resultMap;
+	@Override
+	public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent requestEvent, Context context) {
+		String path = requestEvent.getRequestContext().getHttp().getPath();
+		String method = requestEvent.getRequestContext().getHttp().getMethod();
+
+		// Check if the request is for the /hello endpoint
+		if ("GET".equals(method) && "/hello".equals(path)) {
+			return buildResponse(200, "Hello from Lambda");
+		} else {
+			// Handle all other paths with a 400 error
+			return buildResponse(400, String.format("Bad request syntax or unsupported method. Request path: %s. HTTP method: %s", path, method));
+		}
+	}
+
+	private APIGatewayV2HTTPResponse buildResponse(int statusCode, String message) {
+		Map<String, Object> resultMap = new HashMap<>();
+		resultMap.put("statusCode", statusCode);
+		resultMap.put("message", message);
+
+		return APIGatewayV2HTTPResponse.builder()
+				.withStatusCode(statusCode)
+				.withHeaders(Map.of("Content-Type", "application/json"))
+				.withBody(resultMap.toString()) // You might want to use a JSON library to convert this properly
+				.build();
 	}
 }
