c82f6ed69895e73fab14c05620a4e5ac8b411be2
Harshil Kothari <harshil_kothari@epam.com>
2024-11-06 16:26:22 +0530

    4

--- task04/jsrc/main/java/com/task04/SnsHandler.java
+++ task04/jsrc/main/java/com/task04/SnsHandler.java
@@ -1,43 +1,41 @@
 package com.task04;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.syndicate.deployment.annotations.events.SnsEventSource;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.resources.DependsOn;
+import com.syndicate.deployment.model.ResourceType;
 import com.syndicate.deployment.model.RetentionSetting;
-import com.amazonaws.services.lambda.runtime.events.SNSEvent;
-import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@LambdaHandler(
-    lambdaName = "sns_handler",
-	roleName = "sns_handler-role",
-	isPublishVersion = true,
-	aliasName = "learn",
-	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+@LambdaHandler(lambdaName = "sns_handler",
+		roleName = "sns_handler-role",
+		isPublishVersion = true,
+		aliasName = "learn",
+		logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
-//public class SnsHandler implements RequestHandler<Object, Map<String, Object>> {
-//
-//	public Map<String, Object> handleRequest(Object request, Context context) {
-//		System.out.println("Hello from lambda");
-//		Map<String, Object> resultMap = new HashMap<String, Object>();
-//		resultMap.put("statusCode", 200);
-//		resultMap.put("body", "Hello from Lambda");
-//		return resultMap;
-//	}
-//}
-public class SnsHandler implements RequestHandler<SNSEvent, String> {
+@SnsEventSource(
+		targetTopic="lambda_topic"
+)
+@DependsOn(
+		name = "lambda_topic",
+		resourceType = ResourceType.SNS_TOPIC
+)
+
+public class SnsHandler implements RequestHandler<Object, Map<String, Object>> {
 
 	@Override
-	public String handleRequest(SNSEvent event, Context context) {
-		event.getRecords().forEach(record -> {
-			String message = record.getSNS().getMessage();
-			context.getLogger().log("Received SNS message: " + message);
-		});
-		JSONObject response = new JSONObject();
-		response.put("statusCode", 200);
-		response.put("body", "SNS message logged successfully");
-		return response.toString();
+	public Map<String, Object> handleRequest(Object request, Context context) {
+		LambdaLogger lambdaLogger=context.getLogger();
+		lambdaLogger.log(request.toString());
+		System.out.println("Hello from lambda");
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		resultMap.put("statusCode", 200);
+		resultMap.put("body", "Hello from Lambda");
+		return resultMap;
 	}
 }
